93c5ed2f55ad9c1da39ce48f31055d072bc9d1ce
zlrd10z <64948473+zlord10z@users.noreply.github.com>
2024-06-28 21:00:58 +0200

    Update changeTextColor.py
    Add some font colors

--- Tracker software/gui/changeTextColor.py
+++ Tracker software/gui/changeTextColor.py
@@ -6,6 +6,8 @@ BG_GREEN = "\033[48;5;10m"
 BG_GREY = "\033[47m"
 FONT_BLACK = "\033[30m"
 FONT_BLUE = "\033[38;5;93m"
+FONT_YELLOW = "\033[38;5;226m"
+FONT_GREEN = "\033[38;5;10m"
 
 # Change char's background color:
 def changeStringBgColor(color, text):
@@ -25,5 +27,11 @@ def changeStringFontColor(color, text):
 		colored_string = (f"{FONT_BLUE}{text}{RESET}")
 	if color == "purple":
 		colored_string = (f"{FONT_PURPLE}{text}{RESET}")
+	if color == "black":
+		colored_string = (f"{FONT_BLACK}{text}{RESET}")
+	if color == "yellow":
+		colored_string = (f"{FONT_YELLOW}{text}{RESET}")
+	if color == "green":
+		colored_string = (f"{FONT_GREEN}{text}{RESET}")
 	return colored_string
 
